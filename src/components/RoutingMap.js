6963c6065278f5f018fdf69bf7dc9283bfda1bf6
David Perez <kitsunebrave@gmail.com>
2020-03-29 19:33:45 -0400

    testing

--- src/components/RoutingMap.js
+++ src/components/RoutingMap.js
@@ -10,10 +10,9 @@ const STYLE_ROAD_MAP = 'mapbox://styles/mapbox/streets-v11';
 
 export default class RoutingMap extends React.Component {
   
-  mapRef = React.createRef()
-
   constructor(props) {
     super(props);
+    this.mapRef = React.createRef();
     this.state = {
       lng: -79.4512,
       lat: 43.6568,
@@ -41,34 +40,38 @@ export default class RoutingMap extends React.Component {
 
     //container to put React generated content in
     this.tooltipContainer = document.createElement('div');
-
-    const map = new mapboxgl.Map({
+    const mapContainer = this.mapRef.current;
+    this.map = new mapboxgl.Map({
       hash: 'map',
-      container: this.mapRef.current,
+      container: mapContainer,
       style: STYLE_ROAD_MAP,
       center: [this.state.lng, this.state.lat],
-      zoom: 13
+      zoom: this.zoom
     });
 
     const road_link_info = new mapboxgl.Marker(this.tooltipContainer,{
       offset: [-120, 0]
-    }).setLngLat([0,0]).addTo(map);
+    }).setLngLat([0,0]).addTo(this.map);
 
-    map.on('mousemove', (e) => {
-      const features = map.queryRenderedFeatures(e.point);
+    this.map.on('mousemove', (e) => {
+      const features = this.map.queryRenderedFeatures(e.point);
       road_link_info.setLngLat(e.lngLat);
-      map.getCanvas().style.cursor = features.length ? 'pointer' : '';
+      this.map.getCanvas().style.cursor = features.length ? 'pointer' : '';
       this.setTooltip(features);
     })
 
     const MapboxDirections = window.MapboxDirections
-    map.addControl(
+    this.map.addControl(
       new MapboxDirections({
         accessToken: MAPBOX_ACCESS_TOKEN
       }),
       'top-left'
     );
   };
+
+  __change_traffic(value) {
+
+  }
 
   render() {
     return (
@@ -77,27 +80,35 @@ export default class RoutingMap extends React.Component {
         <div className="roadInfo">
           <div className="information">
             <h2>Road Link Information</h2>
-            <div className='info'>
+            <div className="info">
               <p>Zoom in and hover over a road link for info</p>
             </div>
           </div>
-          <div className='traffic'>
+          <div className="traffic">
             <h3>Traffic</h3>
-            <input type="range" className="traffic-window" name="traffic-window" min="0" max="7" list="tickmarks" value="0"
-              oninput="change_traffic(this.value)" />
-            <label for="traffic-window">0</label>
+            <input
+              type="range"
+              className="traffic-window"
+              name="traffic-window"
+              min="0"
+              max="7"
+              list="tickmarks"
+              defaultValue="0"
+              onInput={this.__change_traffic(this.value)}
+            />
+            <label htmlFor="traffic-window">0</label>
           </div>
         </div>
         <datalist id="tickmarks">
-            <option value="0"></option>
-            <option value="1"></option>
-            <option value="2"></option>
-            <option value="3"></option>
-            <option value="4"></option>
-            <option value="5"></option>
-            <option value="6"></option>
-            <option value="7"></option>
-          </datalist>
+          <option value="0"></option>
+          <option value="1"></option>
+          <option value="2"></option>
+          <option value="3"></option>
+          <option value="4"></option>
+          <option value="5"></option>
+          <option value="6"></option>
+          <option value="7"></option>
+        </datalist>
       </div>
     );
   }
